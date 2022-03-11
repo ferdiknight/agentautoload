8d6b16105b4a14d4acac840e1df726ae28583095
ferdiknight <ferdi@blueferdi.com>
2022-03-12 02:41:38 +0800

    set up

---
+++ src/main/java/com/perfma/demo/agent/autoload/Main.java
@@ -0,0 +1,13 @@
+package com.perfma.demo.agent.autoload;
+
+/**
+ *
+ * @author gongyu "yin.tong@perfma.com"
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test");   
+    }
+    
+}
